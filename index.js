4a2aa2c80754e98dc6344d1469465b285f759ce0
suresh <suresheerf@gmail.com>
2023-06-02 00:10:11 +0530

    fix

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const joinRoom = require('./joinRoom');
-const newMessage = require('./newMessage');
+
 const io = new Server(server);
 
 app.get('/', (req, res) => {
@@ -26,7 +25,7 @@ io.on('connection', (socket) => {
         date: new Date()
       });
     } else {
-      console.log('roooom');
+      console.log('roooom', room);
       socket.to(room).emit('chat message', {
         from: username,
         message: msg,
