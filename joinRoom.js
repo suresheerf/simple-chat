878a5af2f86368e16d5cc2e319a48ddee75d0a1f
suresh <suresheerf@gmail.com>
2023-06-02 00:02:55 +0530

    files deleted

--- joinRoom.js
+++
@@ -1,8 +0,0 @@
-module.exports = (room) => {
-  console.log('message: ' + msg);
-  socket.join(room, {
-    from: socket.id,
-    message: msg,
-    date: new Date()
-  });
-};
--- newMessage.js
+++
@@ -1,8 +0,0 @@
-module.exports = (msg) => {
-  console.log('message: ' + msg);
-  socket.broadcast.emit('chat message', {
-    from: socket.id,
-    message: msg,
-    date: new Date()
-  });
-};
